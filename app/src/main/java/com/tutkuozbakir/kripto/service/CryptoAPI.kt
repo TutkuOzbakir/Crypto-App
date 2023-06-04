badfcfdfe74c833fbe22f73178dcf0b72db99ae5
TutkuOzbakir <tutkuozbakir528@gmail.com>
2023-06-04 11:29:40 +0300

    Retrofit & RxJava

--- app/src/main/java/com/tutkuozbakir/kripto/service/CryptoAPI.kt
+++ app/src/main/java/com/tutkuozbakir/kripto/service/CryptoAPI.kt
@@ -5,7 +5,7 @@ import io.reactivex.Observable
 //import retrofit2.Call
 import retrofit2.http.GET
 
-interface KriptoAPI {
+interface CryptoAPI {
 
 
     //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
--- app/src/main/java/com/tutkuozbakir/kripto/view/MainActivity.kt
+++ app/src/main/java/com/tutkuozbakir/kripto/view/MainActivity.kt
@@ -3,9 +3,15 @@ package com.tutkuozbakir.kripto.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.tutkuozbakir.kripto.adapter.RecyclerViewAdapter
 import com.tutkuozbakir.kripto.databinding.ActivityMainBinding
 import com.tutkuozbakir.kripto.model.CryptoModel
+import com.tutkuozbakir.kripto.service.CryptoAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private val BASE_URL = "https://raw.githubusercontent.com/"
     private lateinit var cryptoList: ArrayList<CryptoModel>
     private lateinit var binding: ActivityMainBinding
+    private var compositeDisposable: CompositeDisposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +27,35 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        compositeDisposable = CompositeDisposable()
+
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
         loadData()
     }
 
+    private fun handleResponse(list: List<CryptoModel>){
+        cryptoList = ArrayList(list)
+        binding.recyclerview.adapter = RecyclerViewAdapter(cryptoList)
+    }
     private fun loadData(){
-        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL).
+            addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(CryptoAPI::class.java)
+
+        compositeDisposable?.add(retrofit.getData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse))
 
         /*
-        val service = retrofit.create(KriptoAPI::class.java)
+
+        --------------------------CALL---------------------------
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        val service = retrofit.create(CryptoAPI::class.java)
        val call = service.getData()
 
         call.enqueue(object : Callback<List<CryptoModel>>{
@@ -48,5 +74,10 @@ class MainActivity : AppCompatActivity() {
 
         })
         */
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
     }
 }
